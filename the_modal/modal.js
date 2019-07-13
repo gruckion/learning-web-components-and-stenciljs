32ede51b6ec8790389b49c9a937cbdec4c6a60c2
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-07-13 10:14:02 +0100

    added title and actions to modal

--- the_modal/modal.js
+++ the_modal/modal.js
@@ -14,19 +14,47 @@ class Modal extends HTMLElement {
                     z-index: 10;
                 }
                 #modal {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
                     position: fixed;
                     top: 15vh;
                     left: 25%;
                     width: 50%;
-                    height: 30rem;
                     z-index: 100;
                     background: white;
                     border-radius: 3px;
                     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
                 }
+                #actions {
+                    border-top: 1px solid #ccc;
+                    padding: 1rem;
+                    display: flex;
+                    justify-content: flex-end;
+                }
+                header {
+                    padding: 1rem;
+                }
+                header h1 {
+                    font-size: 1.25rem;
+                }
+                #actions button {
+                    margin: 0 0.25rem;
+                }
             </style>
             <div id="backdrop"></div>
-            <div id="modal"></div>
+            <div id="modal">
+                <header>
+                    <h1>Please confirm</h1>
+                </header>
+                <section id="main">
+                    <slot></slot>
+                </section>
+                <section id="actions">
+                    <button>Cancel</button>
+                    <button>Okay</button>
+                </section>
+            </div>
         `;
     }
 
