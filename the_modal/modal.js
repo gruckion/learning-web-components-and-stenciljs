6d0aa4f9695c3d57f7bee13edcdbd1252128737b
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-07-13 10:04:11 +0100

    created modal background and basic styles

--- the_modal/modal.js
+++ the_modal/modal.js
@@ -4,7 +4,17 @@ class Modal extends HTMLElement {
         this.attachShadow({ mode: "open" });
         this.shadowRoot.innerHTML = `
             <style>
+                #backdrop {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100vh;
+                    background: rgba(0, 0, 0, 0.75);
+                }
             </style>
+            <div id="backdrop"></div>
+            <div id="modal"></div>
         `;
     }
 
